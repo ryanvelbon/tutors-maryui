0b4a7d48cc535efbdd79cbde2c6522b809598950
Ryan <vellabonelloryan@gmail.com>
2024-06-11 12:32:12 +0200

    refactor: replaces TALL Preset HTML with maryUI components

--- resources/views/livewire/auth/passwords/email.blade.php
+++ resources/views/livewire/auth/passwords/email.blade.php
@@ -30,29 +30,13 @@
                     </div>
                 </div>
             @else
-                <form wire:submit.prevent="sendResetPasswordLink">
-                    <div>
-                        <label for="email" class="block text-sm font-medium text-gray-700 leading-5">
-                            Email address
-                        </label>
+                <x-form wire:submit="sendResetPasswordLink">
 
-                        <div class="mt-1 rounded-md shadow-sm">
-                            <input wire:model.lazy="email" id="email" name="email" type="email" required autofocus class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('email') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:ring-red @enderror" />
-                        </div>
+                    <x-input label="Email address" wire:model="email" autofocus icon="o-envelope" inline />
 
-                        @error('email')
-                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-                        @enderror
-                    </div>
+                    <x-button label="Send password reset link" type="submit" class="mt-6 btn-primary" />
 
-                    <div class="mt-6">
-                        <span class="block w-full rounded-md shadow-sm">
-                            <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-                                Send password reset link
-                            </button>
-                        </span>
-                    </div>
-                </form>
+                </x-form>
             @endif
         </div>
     </div>
