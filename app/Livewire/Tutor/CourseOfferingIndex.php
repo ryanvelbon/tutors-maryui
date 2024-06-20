befa4cb271014822be4a784c1f09317b31071d87
Ryan <vellabonelloryan@gmail.com>
2024-06-20 17:51:06 +0200

    feat: CourseOffering table with pagination & sorting

--- app/Livewire/Tutor/CourseOfferingIndex.php
+++ app/Livewire/Tutor/CourseOfferingIndex.php
@@ -2,14 +2,44 @@
 
 namespace App\Livewire\Tutor;
 
+use App\Models\CourseOffering;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CourseOfferingIndex extends Component
 {
+    use WithPagination;
+
+    public array $sortBy = ['column' => 'price', 'direction' => 'desc'];
+
+    public array $expanded = [];
+
     #[Layout('layouts.panel')]
     public function render()
     {
-        return view('livewire.tutor.course-offering-index');
+        $user = auth()->user();
+
+        $offerings = CourseOffering::query()
+            ->where('tutor_id', $user->tutorId)
+            ->orderBy(...array_values($this->sortBy))
+            ->paginate(10);
+
+        $headers = [
+            ['key' => 'id', 'label' => '#'],
+            ['key' => 'title', 'label' => 'Title'],
+            ['key' => 'status', 'label' => 'Status'],
+            ['key' => 'start_date', 'label' => 'Starts'],
+            ['key' => 'subject.title', 'label' => 'Subject', 'sortable' => false],
+            ['key' => 'level.title', 'label' => 'Level', 'sortable' => false],
+            ['key' => 'total_hours', 'label' => 'Length', 'class' => 'text-center'],
+            ['key' => 'price', 'label' => 'Price', 'class' => 'text-center'],
+            ['key' => 'capacity', 'label' => 'Seats'],
+        ];
+
+        return view('livewire.tutor.course-offering-index', [
+            'headers' => $headers,
+            'offerings' => $offerings,
+        ]);
     }
 }
--- resources/views/livewire/tutor/course-offering-index.blade.php
+++ resources/views/livewire/tutor/course-offering-index.blade.php
@@ -1,7 +1,45 @@
 <div>
-    <x-header title="Cohorts" subtitle="A cohort represents a specific instance of a course being offered at a particular time to a group of students." separator>
+    <x-header title="Cohorts" separator>
+        <x-slot:middle class="!justify-end">
+            <x-input icon="o-bolt" placeholder="Search..." />
+        </x-slot:middle>
         <x-slot:actions>
+            <x-button icon="o-funnel" />
             <x-button label="Offer a course" icon="o-plus" class="btn-primary" :link="route('tutor.courseOfferings.create')" />
         </x-slot:actions>
     </x-header>
+
+    @if($offerings->isEmpty())
+        <div class="mt-12 flex flex-col items-center gap-8">
+            <p>No course offerings found.</p>
+            <x-button label="Offer a course" icon="o-plus" :link="route('tutor.courseOfferings.create')" />
+        </div>
+    @else
+        <x-table :headers="$headers" :rows="$offerings" :sort-by="$sortBy" with-pagination striped wire:model="expanded" expandable>
+            @scope('cell_start_date', $offering)
+                <span class="font-mono">
+                    {{ $offering->start_date->format('Y M d') }}
+                </span>
+            @endscope
+
+            @scope('cell_total_hours', $offering)
+                <span>{{ $offering->total_hours }}</span>
+                <sub>hr</sub>
+            @endscope
+
+            @scope('cell_price', $offering)
+                &euro; {{ $offering->price }}
+            @endscope
+
+            @scope('actions', $offering)
+                <x-button icon="o-calendar-days" class="btn-sm" />
+            @endscope
+
+            @scope('expansion', $offering)
+                <div class="bg-base-200 p-8">
+                    <p>more info</p>
+                </div>
+            @endscope
+        </x-table>
+    @endif
 </div>
