d246f56944f0df40091141850057d93eae6de7d5
Ryan <vellabonelloryan@gmail.com>
2024-06-21 17:59:22 +0200

    style: list lessons as daisyUI table instead

--- resources/views/livewire/tutor/dashboard.blade.php
+++ resources/views/livewire/tutor/dashboard.blade.php
@@ -3,36 +3,45 @@
 <div>
     <x-header title="Dashboard" subtitle="This is the Tutor Dashboard" separator />
 
-    <x-header title="This week" />
-
     <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-        <div class="bg-white shadow p-4 col-span-2">
-            <div class="flex justify-between items-start mb-2">
-                <h2 class="text-2xl font-bold">Lessons</h2>
-                <a href="{{ route('tutor.lessons.index') }}" wire:navigate class="text-xs underline">see all lessons</a>
-            </div>
-            @forelse($lessonsThisWeek as $lesson)
-                <div class="bg-white p-4 space-y-8 border-2 mb-2">
-                    <div class="flex justify-between text-xs">
-                        <div>
-                            {{ $lesson->starts_at->format('D jS M') }}
-                            {{ $lesson->starts_at->format('H:i') }}
-                            -
-                            {{ $lesson->ends_at->format('H:i') }}
-                        </div>
-                        <div>{{ $lesson->status }}</div>
-                    </div>
-                    <div class="flex justify-between text-xs">
-                        <p>{{ $lesson->title }}</p>
-                        <div>{{ $lesson->subject->title }} {{ $lesson->courseOffering->level->code }}</div>
-                    </div>
-                </div>
-            @empty
-                <div>
+        <x-card title="Lessons" class="col-span-2" shadow>
+            <x-slot:menu>
+                <x-button icon="o-plus" tooltip="Schedule a new lesson" class="btn-circle btn-sm" />
+            </x-slot:menu>
+
+            @if($lessonsThisWeek->isEmpty())
+                <div class="py-16 text-center">
                     No lessons scheduled for this week.
                 </div>
-            @endforelse
-        </div>
+            @else
+                <table class="table">
+                    <tbody>
+                        @foreach($lessonsThisWeek as $lesson)
+                            <tr class="hover:bg-base-200/50 ">
+                                <td>
+                                    {{ $lesson->starts_at->format('D jS M') }}
+                                </td>
+                                <td>
+                                    {{ $lesson->starts_at->format('H:i') }} - {{ $lesson->ends_at->format('H:i') }}
+                                </td>
+                                <td>
+                                    {{ $lesson->subject->title }} {{ $lesson->courseOffering->level->code }}
+                                </td>
+                                <td>
+                                    {{ $lesson->status }}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+            <x-slot:actions>
+                <x-button label="See all lessons" :link="route('tutor.lessons.index')" class="btn-sm mx-auto" />
+            </x-slot:actions>
+        </x-card>
+
+        <x-card title="News" shadow>
+        </x-card>
     </div>
 </div>
