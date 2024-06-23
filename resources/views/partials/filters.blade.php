3b47ab42d12cf726331210c2ee91e880eb814c60
Ryan <vellabonelloryan@gmail.com>
2024-06-23 10:17:11 +0200

    feat: disable Level dropdown if no Subject selected

--- resources/views/partials/filters.blade.php
+++ resources/views/partials/filters.blade.php
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-1 md:grid-cols-4 gap-2">
     <x-select label="I want to learn" :options="$subjectOptions" option-label="title" wire:model.live="subjectId" inline />
 
-    <x-select label="Level" :options="$levelOptions" option-label="title" inline />
+    <x-select label="Level" :options="$levelOptions" option-label="title" inline :disabled="empty($subjectId)" />
 
     <x-select label="Locality" :options="$localityOptions" inline />
     {{-- make this a dropdown where user can select multiple localities --}}
